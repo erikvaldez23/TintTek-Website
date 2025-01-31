8084d9ea19a80fba1aba645fde27b76812c66f75
erikvldez23 <erikkvaldez@gmail.com>
2025-01-30 23:07:57 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
-import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Topbar from './components/Topbar';  
-import Hero from './components/Hero';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Topbar from './components/Topbar';
+import Hero from './components/Hero';
 import Testimonials from './components/Testimonials';
 import Services from './components/Services';
 import Contact from './components/Contact';
@@ -11,25 +9,23 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route 
-            path="/TintTek-Website/" 
-            element={
-              <>
-                <Topbar />
-                <Hero />
-                <Services />
-                <Testimonials />
-                <Vision />
-                <Contact />
-                <Footer />
-              </>
-            } 
-          />
-        </Routes>
-      </div>
+    <Router basename="/TintTek-Website"> {/* ✅ Add your repo name here */}
+      <Routes>
+        <Route 
+          path="/" 
+          element={
+            <>
+              <Topbar />
+              <Hero />
+              <Services />
+              <Testimonials />
+              <Vision />
+              <Contact />
+              <Footer />
+            </>
+          } 
+        />
+      </Routes>
     </Router>
   );
 }
