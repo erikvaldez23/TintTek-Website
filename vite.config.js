b57c8b73511f16b40e811e8e08acff3c6c232007
erikvldez23 <erikkvaldez@gmail.com>
2025-04-13 21:49:40 -0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  base: '/TintTek-Website/', // your base URL
-  server: {
-    historyApiFallback: true,  // This will catch refresh errors and redirect to index.html
-  },
+  base: '/', // Change this to '/' for the custom domain
 });
